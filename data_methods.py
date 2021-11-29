f5b945b5ab44dc11714bbb428cb0c430874f3662
MikkelMathiasen23 <51484172+MikkelMathiasen23@users.noreply.github.com>
2021-11-29 15:59:14 +0100

    Add files via upload

---
+++ data_methods.py
@@ -0,0 +1,197 @@
+import pandas as pd
+import numpy as np
+import networkx as nx
+from itertools import cycle
+from fa2 import ForceAtlas2
+
+import plotly.graph_objects as go
+import plotly.express as px
+
+
+def graph_preprocessing(network, attribute):
+    """
+    network:    unpickled network graph
+    attribute:  string
+    """
+
+    palette = cycle(px.colors.qualitative.Alphabet)
+
+    attribute_group = np.unique([
+        *list(dict(nx.get_node_attributes(network, attribute)).values())
+    ]).tolist()
+    attribute_to_number = dict(
+        zip(attribute_group, np.arange(len(attribute_group))))
+    culture_colors = [
+        attribute_to_number[attributes[attribute]]
+        for n, attributes in network.nodes(data=True)
+    ]
+
+    palette = cycle(px.colors.qualitative.Alphabet)
+    attribute_color_lst = [
+        next(iter(palette)) for i in range(len(attribute_group))
+    ]
+    attribute_color_dict = dict(zip(attribute_group, attribute_color_lst))
+
+    node_sizes = [int(network.degree(n)) for n in network.nodes()]
+    max_node_size = np.max(node_sizes)
+    min_node_size = np.min(node_sizes)
+    degree_dict_plotly = dict(
+        zip(list(network.nodes()),
+            [(50 - 10) / (max_node_size - min_node_size) *
+             (node_size - max_node_size) + 50 for node_size in node_sizes]))
+
+    #edge_sizes = list(nx.get_edge_attributes(network, 'weight'))
+    edge_sizes = [network.edges()[edge]['weight'] for edge in network.edges()]
+    max_edge_size = np.max(edge_sizes)
+    min_edge_size = np.min(edge_sizes)
+    edge_width_dict_plotly = dict(
+        zip(list(network.edges()),
+            [(15 - 2) / (max_edge_size - min_edge_size) *
+             (edge_size - max_edge_size) + 15 for edge_size in edge_sizes]))
+
+    node_dict = network.nodes(data=True)
+    attribute_edge_colors = []
+
+    for edge in network.edges():
+        attribute_edge_colors.append(
+            attribute_to_number[node_dict[edge[0]][attribute]])
+
+    return attribute_group, attribute_color_dict, degree_dict_plotly, edge_width_dict_plotly
+
+
+def make_edge(x, y, text, width, opacity):
+    return go.Scatter(x=x,
+                      y=y,
+                      line=dict(width=width, color='cornflowerblue'),
+                      hoverinfo='text',
+                      text=([text]),
+                      mode='lines',
+                      showlegend=False,
+                      opacity=opacity)
+
+
+def make_figure(network, attribute, positions):
+
+    attribute_group, attribute_color_dict, degree_dict_plotly, edge_width_dict_plotly = graph_preprocessing(
+        network, attribute)
+    node_dict = network.nodes(data=True)
+
+    edge_widths = nx.get_edge_attributes(network, 'weight')
+    alphas = (0.8 - 0.05) / (np.max(list(edge_widths.values())) - np.min(
+        list(edge_widths.values()))) * (list(edge_widths.values()) - np.max(
+            list(edge_widths.values()))) + 0.8
+    opacity_dict = dict(zip(list(edge_widths.keys()), alphas))
+
+    # Create edge traces
+    edge_traces = []
+    for edge in network.edges():
+
+        if network.edges()[edge]['weight'] > 0:
+            char_1 = edge[0]
+            char_2 = edge[1]
+            x0, y0 = positions[char_1]
+            x1, y1 = positions[char_2]
+            text = char_1 + '--' + char_2 + ': ' + str(
+                network.edges()[edge]['weight'])
+            opacity = opacity_dict[edge]
+
+            trace = make_edge(
+                [x0, x1, None],
+                [y0, y1, None],
+                text,
+                width=edge_width_dict_plotly[
+                    edge],  #network.edges()[edge]['weight']*0.2
+                opacity=opacity)
+
+            edge_traces.append(trace)
+
+    # Create node traces
+    node_traces = []
+    for group in attribute_group:
+        node_trace = go.Scatter(x=[],
+                                y=[],
+                                text=[],
+                                textposition="top center",
+                                textfont_size=10,
+                                mode='markers+text',
+                                showlegend=True,
+                                hoverinfo='text',
+                                name=group,
+                                hovertext=[],
+                                meta=[],
+                                customdata=[],
+                                marker=dict(color=[], size=[], line=None))
+
+        for node, attributes in network.nodes(data=True):
+            if attributes[attribute] == group:
+
+                x, y = positions[node]
+                node_trace['x'] += tuple([x])
+                node_trace['y'] += tuple([y])
+                color_key = node_dict[node][attribute]
+                node_trace['marker']['color'] += tuple(
+                    [attribute_color_dict[color_key]])
+                node_trace['marker']['size'] += tuple(
+                    [degree_dict_plotly[node]])
+                node_trace['hovertext'] += tuple([node.replace(
+                    '_', ' ')])  #tuple([attributes['text']])
+                node_trace['text'] += tuple([node.replace('_', ' ')])
+                node_trace['customdata'] += tuple([attributes['text']])
+                node_trace['meta'] += tuple([attributes['thumbnail']])
+        node_traces.append(node_trace)
+
+    return edge_traces, node_traces
+
+
+def create_thumbnail(img_src):
+    # Create figure
+    fig = go.Figure()
+
+    # Constants
+    img_width = 200
+    img_height = 300
+    scale_factor = 0.5
+
+    # Add invisible scatter trace.
+    # This trace is added to help the autoresize logic work.
+    fig.add_trace(
+        go.Scatter(x=[0, img_width * scale_factor],
+                   y=[0, img_height * scale_factor],
+                   mode="markers",
+                   marker_opacity=0))
+
+    # Configure axes
+    fig.update_xaxes(visible=False, range=[0, img_width * scale_factor])
+
+    fig.update_yaxes(
+        visible=False,
+        range=[0, img_height * scale_factor],
+        # the scaleanchor attribute ensures that the aspect ratio stays constant
+        scaleanchor="x")
+
+    # Add image
+    fig.add_layout_image(
+        dict(x=0,
+             sizex=img_width * scale_factor,
+             y=img_height * scale_factor,
+             sizey=img_height * scale_factor,
+             xref="x",
+             yref="y",
+             opacity=1.0,
+             layer="below",
+             sizing="stretch",
+             source=img_src))
+
+    # Configure other layout
+    fig.update_layout(
+        width=img_width * scale_factor,
+        height=img_height * scale_factor,
+        margin={
+            "l": 0,
+            "r": 0,
+            "t": 0,
+            "b": 0
+        },
+    )
+
+    return fig
---
+++ network_app.py
@@ -0,0 +1,226 @@
+import json
+import networkx as nx
+import pandas as pd
+
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import plotly.graph_objects as go
+
+from data_methods import make_figure
+from data_methods import create_thumbnail
+from fa2 import ForceAtlas2
+
+# from skimage import io
+import random
+import os
+import gunicorn
+from whitenoise import WhiteNoise
+
+from PIL import Image
+import requests
+import pickle
+from io import BytesIO
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+config_thumbnail = {'staticPlot': True}
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+styles = {'pre': {'border': 'thin lightgrey solid', 'overflowY': 'scroll'}}
+
+# os.chdir('network_app/')
+network = nx.read_gpickle("got_G.gpickle")
+with open('link_partition.pickle', 'rb') as handle:
+    links = pickle.load(handle)
+
+
+def compute_positions(network):
+
+    forceatlas2 = ForceAtlas2(
+        # Behavior alternatives
+        outboundAttractionDistribution=True,  # Dissuade hubs
+        edgeWeightInfluence=1,
+
+        # Performance
+        jitterTolerance=0.2,  # Tolerance
+        barnesHutOptimize=True,
+        barnesHutTheta=0.5,
+        multiThreaded=False,  # NOT IMPLEMENTED
+
+        # Tuning
+        scalingRatio=10.0,
+        strongGravityMode=False,
+        gravity=0.01,
+
+        # Log
+        verbose=True)
+
+    positions = forceatlas2.forceatlas2_networkx_layout(G=network,
+                                                        pos=None,
+                                                        iterations=2000)
+    return positions
+
+
+random.seed(1)
+positions = compute_positions(network)
+attribute = 'religion'
+
+edge_traces, node_traces = make_figure(network, attribute, positions)
+
+layout = go.Layout(
+    paper_bgcolor='rgba(0,0,0,0)',  # transparent background
+    plot_bgcolor='rgba(0,0,0,0)',  # transparent 2nd background
+    xaxis={
+        'showgrid': False,
+        'zeroline': False
+    },  # no gridlines
+    yaxis={
+        'showgrid': False,
+        'zeroline': False
+    },  # no gridlines
+)
+
+#Create figure
+fig = go.Figure(layout=layout)
+thumbnail = go.Figure(layout=layout)
+
+# Add all edge traces
+for trace in edge_traces:
+    fig.add_trace(trace)  # Add node trace
+for trace in node_traces:
+    fig.add_trace(trace)
+
+fig.update_xaxes(showticklabels=False)
+fig.update_yaxes(showticklabels=False)
+fig.update_layout(clickmode='event+select')
+
+app.layout = html.Div([
+    dcc.Graph(id='computed_figure', figure=fig, style={'height': '70vh'}),
+    html.Div(
+        className='row',
+        children=[
+            html.Div([
+                dcc.Markdown("""
+                **Season selection**
+                Select a season to view the corresponding network or select overall to view the built network for all seasons combined.
+            """),
+                dcc.Dropdown(id="com_dropdown_menu",
+                             options=[{
+                                 'label': 'Community 0',
+                                 'value': '0'
+                             }, {
+                                 'label': 'Community 1',
+                                 'value': '1'
+                             }, {
+                                 'label': 'Community 2',
+                                 'value': '2'
+                             }, {
+                                 'label': 'Community 3',
+                                 'value': '3'
+                             }, {
+                                 'label': 'Community 4',
+                                 'value': '4'
+                             }, {
+                                 'label': 'Community 5',
+                                 'value': '5'
+                             }],
+                             value='0'),
+                html.Pre(id='com_dropdown', style=styles['pre'])
+            ],
+                     className='three columns'),
+            html.Div([
+                dcc.Markdown("""
+                **Graph attribute overlay**
+                Select attribute overlay of the graph.
+            """),
+                dcc.Dropdown(id="attribute_dropdown_menu",
+                             options=[{
+                                 'label': 'Allegiance',
+                                 'value': 'allegiance'
+                             }, {
+                                 'label': 'Religion',
+                                 'value': 'religion'
+                             }, {
+                                 'label': 'Culture',
+                                 'value': 'culture'
+                             }],
+                             value='religion'),
+                html.Pre(id='attribute_dropdown', style=styles['pre']),
+            ],
+                     className='three columns'),
+            html.Div([
+                dcc.Markdown("""
+                **Click Data**
+                Click on points in the graph to show meta data and the characters most frequently used words according to Term Frequency Inverse Document Count.
+            """),
+                html.Pre(id='click-data', style=styles['pre']),
+            ],
+                     className='three columns'),
+            html.Div(
+                [
+                    dcc.Markdown("""
+                **Character Image**
+            """),
+                    dcc.Graph(id='selected-data', figure=thumbnail),
+                    #html.Pre(id='selected-data', style=styles['pre']),
+                ],
+                className='three columns'),
+        ])
+])
+
+
+@app.callback(Output('computed_figure', 'figure'), [
+    Input('com_dropdown_menu', 'value'),
+    Input('attribute_dropdown_menu', 'value')
+])
+def display_figure(com_dropdown_menu, attribute_dropdown_menu):
+
+    network = nx.read_gpickle("got_G.gpickle")
+    network = network.subgraph(links[int(com_dropdown_menu)])
+
+    attribute = attribute_dropdown_menu
+    random.seed(1)
+    positions = compute_positions(network)
+
+    edge_traces, node_traces = make_figure(network, attribute, positions)
+
+    # Create figure
+    fig = go.Figure(layout=layout)
+
+    # Add all edge traces
+    for trace in edge_traces:
+        fig.add_trace(trace)  # Add node trace
+    for trace in node_traces:
+        fig.add_trace(trace)
+
+    fig.update_xaxes(showticklabels=False)
+    fig.update_yaxes(showticklabels=False)
+    fig.update_layout(clickmode='event+select')
+
+    return fig
+
+
+@app.callback(
+    [Output('click-data', 'children'),
+     Output('selected-data', 'figure')], Input('computed_figure', 'clickData'))
+def display_click_data(clickData):
+    if clickData is not None:
+        path = clickData['points'][0]['meta']
+        response = requests.get(path)
+        img = Image.open(BytesIO(response.content))
+        thumbnail = create_thumbnail(img)
+
+        return clickData['points'][0]['customdata'], thumbnail
+
+    path = "https://static.wikia.nocookie.net/gameofthrones/images/c/c8/Iron_throne.jpg/revision/latest/scale-to-width-down/334?cb=20131005175755"
+    response = requests.get(path)
+    img = Image.open(BytesIO(response.content))
+    thumbnail = create_thumbnail(img)
+    return json.dumps(clickData, indent=2), thumbnail
+
+
+server = app.server
+server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/')
